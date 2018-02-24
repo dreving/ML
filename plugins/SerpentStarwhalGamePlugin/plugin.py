b9edff3deccf00dde58c4fc1b044a4ed49ab2ad8
dreving <drevinwarrior@gmail.com>
2018-02-23 22:25:39 -0500

    Initial commit

---
+++ plugins/SerpentStarwhalGamePlugin/plugin.py
@@ -0,0 +1,28 @@
+import offshoot
+
+
+class SerpentStarwhalGamePlugin(offshoot.Plugin):
+    name = "SerpentStarwhalGamePlugin"
+    version = "0.1.0"
+
+    libraries = []
+
+    files = [
+        {"path": "serpent_Starwhal_game.py", "pluggable": "Game"}
+    ]
+
+    config = {
+        "fps": 2
+    }
+
+    @classmethod
+    def on_install(cls):
+        print("\n\n%s was installed successfully!" % cls.__name__)
+
+    @classmethod
+    def on_uninstall(cls):
+        print("\n\n%s was uninstalled successfully!" % cls.__name__)
+
+
+if __name__ == "__main__":
+    offshoot.executable_hook(SerpentStarwhalGamePlugin)
